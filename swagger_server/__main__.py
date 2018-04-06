56d57ca56d1bf448bff57d7dbedf9883dde33c08
lfdantoni <lfdantoni@gmail.com>
2018-04-06 14:55:22 -0300

    Test

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,14 +3,24 @@
 import connexion
 
 from swagger_server import encoder
+# from flask import Flask
 
+# application = Flask(__name__)
+# app = application
+
+# @app.route("/")     
+# def hello():         
+# 	return "Hello World!"
+
+application = connexion.App(__name__, specification_dir='./swagger/')
+application.app.json_encoder = encoder.JSONEncoder
+application.add_api('swagger.yaml', arguments={'title': 'Swagger Petstore'})
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Swagger Petstore'})
-    app.run(port=8080)
-
+    return app
 
 if __name__ == '__main__':
-    main()
+    application.run(port=8080)
---
+++ swagger_server/application.py
@@ -0,0 +1,9 @@
+from flask import Flask
+application = Flask(__name__)  # Change assignment here
+
+@application.route("/")        # Change your route statements
+def hello():         
+	return "Hello World!"
+
+if __name__ == "__main__":         
+	application.run() 		   # Change all other references to 'app'
