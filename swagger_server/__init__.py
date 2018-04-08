e79685e6b8d9c2b922e7379f764986130b9d8ac8
lfdantoni <lfdantoni@gmail.com>
2018-04-08 14:42:51 -0300

    change swagger.yaml

--- swagger_server/__init__.py
+++ swagger_server/__init__.py
@@ -4,4 +4,4 @@ from swagger_server import encoder
 
 application = connexion.App(__name__, specification_dir='./swagger/')
 application.app.json_encoder = encoder.JSONEncoder
-application.add_api('swagger.yaml', arguments={'title': 'Swagger Petstore'})
+application.add_api('swagger_prod.yaml', arguments={'title': 'Swagger Petstore'})
--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -19,7 +19,7 @@ from swagger_server import encoder
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Swagger Petstore'})
+    app.add_api('swagger_dev.yaml', arguments={'title': 'Swagger Petstore'})
     app.run(port=8080)
 
 if __name__ == '__main__':
