66952279eefab61bc3d9ab6f1a1b773fcbde5cf3
lfdantoni <lfdantoni@gmail.com>
2018-04-08 14:54:38 -0300

    change swagger.yaml

--- swagger_server/controllers/photo_controller.py
+++ swagger_server/controllers/photo_controller.py
@@ -6,20 +6,7 @@ from swagger_server import util
 import boto3
 
 
-def upload_file(userId, additionalMetadata=None, file=None):  # noqa: E501
-    # """uploads an image
-
-    #  # noqa: E501
-
-    # :param userId: ID of user to update
-    # :type userId: int
-    # :param additionalMetadata: Additional data to pass to server
-    # :type additionalMetadata: str
-    # :param file: file to upload
-    # :type file: werkzeug.datastructures.FileStorage
-
-    # :rtype: ApiResponse
-    # """
+def upload_file(file, userId=0, additionalMetadata=None):
     lis = []
     s3 = boto3.resource('s3')
     for bucket in s3.buckets.all():
@@ -28,7 +15,6 @@ def upload_file(userId, additionalMetadata=None, file=None):  # noqa: E501
     client = boto3.client('rekognition', region_name='us-west-2')
     response = client.detect_faces(
         Image={
-            # 'Bytes': b'bytes',
             'Bytes': file.read()
         },
         Attributes=[
